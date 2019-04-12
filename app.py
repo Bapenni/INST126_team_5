8de7306b0eed71bfc9bc9297599aa1b8737174b3
Bapenni <44118863+Bapenni@users.noreply.github.com>
2019-04-11 22:02:03 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import requests
+
+
+city = input('Enter a valid city: ')
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=e9926d9cabaa85df3a3592b64a40691b&units=imperial'.format(city)
+
+request = requests.get(url)
+
+data = request.json()
+
+# print(request)
+#
+# print(data)
+#
+print(url)
+
+print('*~*~*~*~*~*~*~*')
+# variable storage
+temp = data['main']['temp']
+wind_speed = data['wind']['speed']
+
+print('The temperature in ' + city + ' is: ' + str(temp) + ' Farenheit')
+print('The windspeed is: ' + str(wind_speed) + ' mph')
